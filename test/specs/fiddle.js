4092380dcdca244fe659a671c513b80c4456a1ca
xcold8 <haim@phanatic.io>
2017-05-03 15:16:53 +0300

    New webdriver training

--- test/specs/fiddle.js
+++ test/specs/fiddle.js
@@ -14,18 +14,13 @@ driver.init().then(function(){
 
 	console.log('driver initiated...');
 	driver
-		.url('http://www.basparts.com')
+		.url('http://www.google.com')
 		.pause(3500)
-		.setValue('#search-input', '21300289')
-		.pause(2500)
-		.click('#part-search-button')
+		.setValue('input[id="lst-ib"]', 'djdo@walla.com')
 		.pause(3000)
-		.click('#searchContainer .newlistingblock .data a')
+		.click('button[id="_fZl"]')
+		.pause(4000)
+		.getAttribute('=Open Tour', 'href').click()
 		.pause(3500)
-		.driver.end();
-
-
-		//.getText('#main div.str3s_txt').then(function(text_ex){
-			//console.log(text_ex);
-		//});
+ 		.driver.end();
 	});
